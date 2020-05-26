0798abfff30fc49d5d90b79fbf44dc1d637cd655
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-05-26 15:46:02 +0200

    essai lib OCR tesseract (tess4j)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import nu.pattern.OpenCV;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
@@ -16,15 +18,33 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args){
+        Tesseract tess = new Tesseract();
+        tess.setDatapath("C:\\Users\\robin.jesson\\Downloads\\tesseract-2.00.fra.tar\\tesseract-2.00.fra\\tessdata");
+        tess.setLanguage("fra");
+        tess.setTessVariable("user_defined_dpi", "70");
+
         OpenCV.loadLocally();
         String imgSrc="C:\\Users\\robin.jesson\\Desktop\\cvr.png";
         Mat img = loadImage(imgSrc);
         LinkedList<Mat> rects = TextDetection.getTextBlock(img);
         int i=0;
-        for(Mat roi : rects){
-            saveImage(roi, ".\\roi\\nor_"+ i++ + ".png");
+        for(Mat roi : rects) {
+            //saveImage(roi, ".\\roi\\nor_" + i++ + ".png");
             Mat rot = Deskewing.deskew(roi);
-            saveImage(rot, ".\\roi\\rot_"+ i++ + ".png");
+            //saveImage(rot, ".\\roi\\rot_" + i++ + ".png");
+
+        }
+
+        File f = new File("./roi");
+        i=0;
+        for(File file : f.listFiles()){
+            System.out.println(file);
+            try {
+                String text = tess.doOCR(file);
+                System.out.println(text);
+            } catch (TesseractException e) {
+                e.printStackTrace();
+            }
         }
     }
 
