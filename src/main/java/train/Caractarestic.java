b3a2e5ffc8c886b147ef726c2e00dcd452436e59
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-06-17 20:40:51 +0200

    test du knn opencv ok

--- src/main/java/train/Caractarestic.java
+++ src/main/java/train/Caractarestic.java
@@ -43,13 +43,20 @@ public class Caractarestic {
     public static void main(String[] args) throws Exception {
         OpenCV.loadLocally();
         File folder = Paths.get("C:\\Users\\robin.jesson\\Documents\\letters\\ponct\\").toFile();
-        Mat traindata = new Mat(new Size(16,folder.listFiles().length),CvType.CV_32F);
-        Mat labels = new Mat(new Size(1,folder.listFiles().length),CvType.CV_32F);
+        int nbFile = 0;
+        for(File f : folder.listFiles()){
+            for(File pic : f.listFiles()){
+                nbFile++;
+            }
+        }
+        Mat traindata = new Mat(new Size(16,nbFile),CvType.CV_32F);
+        Mat labels = new Mat(new Size(1,nbFile),CvType.CV_32F);
+        int line = 0;
         for(File f : folder.listFiles()){
             int charName = (int)Ponct.valueOf(f.getName()).getC();
-            int line = 0;
-            labels.put(line,0,charName);
+
             for(File pic : f.listFiles()){
+                labels.put(line,0,charName);
                 try {
                     Mat im = Image.loadImage(pic.getAbsolutePath());
                     int[] vec = getVector(im,charName,8);
@@ -58,20 +65,21 @@ public class Caractarestic {
                     }
                 }
                 catch (NotFileException ex){/* do nothing */}
+                line++;
             }
-            line++;
+
         }
         KNearest knn = KNearest.create();
         knn.train(traindata, Ml.ROW_SAMPLE, labels);
         knn.save("ocr.txt");
         Mat res = new Mat();
         Mat testdata = new Mat(new Size(16,folder.listFiles().length),CvType.CV_32F);
-        int[] vec = new int[]{0,20,35,25,47,56,52,24,8,30,44,35,40,50,40,9};
+        int[] vec = new int[]{2,53,55,10,54,64,64,54,65,64,4,60,13,6,60,13};
         for(int i=0;i<vec.length;i++){
             testdata.put(0,i,vec[i]);
         }
 
-        float p = knn.findNearest(testdata,3,res);
+        float p = knn.findNearest(testdata,5,res);
         System.out.println(res.dump());
     }
 }
